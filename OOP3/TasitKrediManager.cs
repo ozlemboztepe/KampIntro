d862ce7b1df4c0c9aaf7f89ba6018347d0024f17
Özlem <ozlem_boztepe@yahoo.com>
2021-02-12 21:26:48 +0300

    5. ders Sonu

---
+++ OOP3/TasitKrediManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class TasitKrediManager : IKrediManager
+    {
+        public void BiseYap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hesapla()
+        {
+            //
+            //
+            
+            Console.WriteLine("Taşıt kredisi ödeme planı hesaplandı");
+
+        }
+    }
+}
